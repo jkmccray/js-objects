a4f938f2c2271ec12b6bf8697509d14c7160c489
Jacquelyn M <jacquelynkmccray@gmail.com>
2019-07-17 10:34:57 -0500

    Added notes from going over chapter in class

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 // Object behavior
 // You can define a function as a key. These are called methods instead of properties.
+// Can replace function keyword with arrow syntax, as long as you are not using "this".
+// Think of functions as defining or changing the behavior of an object.
 const rufusTheDog = {
   age: 4,                 // Property
   species: "Dalmatian",   // Property
@@ -24,6 +26,8 @@ brutusTheDog.bark("mailman")
 brutusTheDog.bark("leaf")
 brutusTheDog.bark("child")
 
+brutusTheDog.age = 5 // Modified the object's state by changing one of the values 
+
 // Intro to "this"
 // In the code below, "this" takes the place of "cletusTheDog". 
 // If the name of the object were to change to "myDogCletus", you do not have to change multiple lines of code, only the variable name.
@@ -32,11 +36,11 @@ const cletusTheDog = {
   species: "Dalmatian",
   contentsOfStomach: [],
   eat: function (item) {
-      this.contentsOfStomach.push(item)
+      this.contentsOfStomach.push(item) // When using "this" in an object, the method cannot be an arrow function. It has to have the function keyword.
       debugger
   },
   puke: function () {
-      this.contentsOfStomach.length = 0
+      this.contentsOfStomach.length = 0 // Easiest way to empty out an array
   },
   bark: function (something) {
       window.alert(`Cletus barks 'WOOF!' at ${something}`)
